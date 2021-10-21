6d71cb55bc6bf82105952f585b9919b13be5f84a
patrick hrabos <phrabos@gmail.com>
2021-10-21 15:32:48 -0700

    WIP initial setup

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+	Button,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+	Alert,
+	TouchableOpacity,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [zipInput, setZipInput] = useState('');
+	const options = [
+		{ key: 'insert', text: 'add zip code to database' },
+		{ key: 'delete', text: 'delete zip code from database' },
+		{ key: 'has', text: 'is zip code in database' },
+		{ key: 'display', text: 'get all zip codes' },
+	];
+
+	const onSubmit = () => {
+		Alert.alert(zipInput);
+		setZipInput('');
+	};
+	return (
+		<View style={styles.container}>
+			<Text>Zip Code</Text>
+			<TextInput
+				style={{
+					height: 40,
+					borderColor: 'gray',
+					borderWidth: 1,
+				}}
+				defaultValue={zipInput}
+				placeholder="enter zip code here"
+				returnKeyType="done"
+				onSubmitEditing={onSubmit}
+				onChangeText={(text) => setZipInput(text)}
+			/>
+			{options.map((option, i) => (
+				<TouchableOpacity onPress={onSubmit}>
+					<Text>{option.text}</Text>
+				</TouchableOpacity>
+			))}
+			<Button title="add to database" onPress={onSubmit}>
+				Add to Database
+			</Button>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
